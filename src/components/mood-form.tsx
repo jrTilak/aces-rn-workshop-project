c5d2c5961f91f188ec77cd826014755f15bde9f2
jrtilak <contact@jrtilak.dev>
2026-07-05 07:30:34 +0545

    update

--- src/components/mood-form.tsx
+++ src/components/mood-form.tsx
@@ -2,11 +2,19 @@ import { Emoji } from "@/components/emoji";
 import { P } from "@/components/p";
 import { TextArea } from "@/components/textarea";
 import { EMOJIS } from "@/constants/emojis";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useState } from "react";
 import { Button, ScrollView, StyleSheet, View } from "react-native";
 
+type SavedMood = {
+  emoji: number;
+  text: string;
+  date: string;
+};
+
 export default function MoodForm() {
   const [selectedEmoji, setSelectedEmoji] = useState<number>();
+  const [text, setText] = useState("");
   return (
     <View style={styles.formContainer}>
       <P>How are you feeling today?</P>
@@ -28,9 +36,40 @@ export default function MoodForm() {
       </ScrollView>
       <View>
         <P>Notes</P>
-        <TextArea />
+        <TextArea
+          value={text}
+          onChangeText={(text) => {
+            setText(text);
+          }}
+        />
       </View>
-      <Button title="Save" />
+      <Button
+        onPress={async () => {
+          if (!selectedEmoji) return;
+
+          const existing = await AsyncStorage.getItem("moods");
+
+          let data: SavedMood[] = [];
+
+          if (existing) {
+            data = JSON.parse(existing);
+          }
+
+          data.push({
+            date: new Date().toISOString(),
+            emoji: selectedEmoji,
+            text: text,
+          });
+
+          await AsyncStorage.setItem("moods", JSON.stringify(data));
+
+          console.log(await AsyncStorage.getItem("moods"));
+
+          setSelectedEmoji(undefined);
+          setText("");
+        }}
+        title="Save"
+      />
     </View>
   );
 }
